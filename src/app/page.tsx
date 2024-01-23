02ee70efa476372f4d8a8e1c30f5716526a0c0c7
nitinjain <nitinjain.work1991@gamil.com>
2024-01-23 16:48:14 +0530

    uptade new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import JoinRoom from "./screens/JoinRoom";
-import ControlSetups from "./screens/ControlSetups";
+// import ControlSetups from "./screens/ControlSetups";
 
 const HomePage = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<JoinRoom />} />
-        {/* <Route path="/kjgbhjhgbjhgjh" element={<ControlSetups />} /> */}
+        {/* <Route path="/ControlSetups" element={<ControlSetups />} /> */}
       </Routes>
     </Router>
   );
